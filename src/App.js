98a59b261c4a47ed1b4fc3ee3e304297a6c0adc5
Fenvrir <dragonofhokage@gmail.com>
2022-04-09 17:19:48 +0700

    added react lazy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import News from "./components/News/News";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfileSettings from "./components/ProfileSettings/ProfileSettings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import MusicContainer from "./components/Music/MusicContainer";
@@ -15,16 +12,17 @@ import { connect } from "react-redux";
 import { initializeApp } from "./redux/app-reducer";
 import Preloader from "./components/Users/Preloader";
 
+const ProfileContainer = React.lazy(() =>import("./components/Profile/ProfileContainer"));
+const DialogsContainer = React.lazy(() =>import("./components/Dialogs/DialogsContainer"));
+const UsersContainer = React.lazy(() =>import("./components/Users/UsersContainer"));
 class App extends React.Component {
-
   componentDidMount() {
     this.props.initializeApp();
- 
   }
 
   render() {
-    if(!this.props.initialized) {
-      return <Preloader/>
+    if (!this.props.initialized) {
+      return <Preloader />;
     }
     return (
       <BrowserRouter>
@@ -32,40 +30,45 @@ class App extends React.Component {
           <HeaderContainer />
           <NavBar state={this.props.store} />
           <div className="app-wrapper__content">
-            <Routes>
-              <Route
-                path="profile/*"
-                element={<ProfileContainer store={this.props.store} />} />
-              <Route
-                path="dialogs/*"
-                element={<DialogsContainer store={this.props.store} />} />
-              <Route path="music"
-                element={<MusicContainer store={this.props.store} />} />
-              <Route path="news" element={<News store={this.props.store} />} />
-              <Route path="login" element={<Login />} />
-              <Route
-                path="settings"
-                element={<ProfileSettings
-                  store={this.props.store} />} />
-              <Route
-                path="friends"
-                element={<FriendsContainer
-                  store={this.props.store} />}
-              />
-              <Route
-                path="users"
-                element={<UsersContainer />}
-              />
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route
+                  path="profile/*"
+                  element={<ProfileContainer store={this.props.store} />}
+                />
+                <Route
+                  path="dialogs/*"
+                  element={<DialogsContainer store={this.props.store} />}
+                />
+                <Route
+                  path="music"
+                  element={<MusicContainer store={this.props.store} />}
+                />
+                <Route
+                  path="news"
+                  element={<News store={this.props.store} />}
+                />
+                <Route path="login" element={<Login />} />
+                <Route
+                  path="settings"
+                  element={<ProfileSettings store={this.props.store} />}
+                />
+                <Route
+                  path="friends"
+                  element={<FriendsContainer store={this.props.store} />}
+                />
+                <Route path="users" element={<UsersContainer />} />
+              </Routes>
+            </Suspense>
           </div>
         </div>
       </BrowserRouter>
     );
   }
-};
+}
 
 let mapStateToProps = (state) => ({
-  initialized: state.app.initialized
-})
+  initialized: state.app.initialized,
+});
 
-export default connect(mapStateToProps, {initializeApp})( App);
+export default connect(mapStateToProps, { initializeApp })(App);
